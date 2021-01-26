e05c79e263e6939d06e88cebbc3633048b173b17
Adriano Ruseler <ruseler@utfpr.edu.br>
2021-01-26 10:57:32 -0300

    Update getmoodle.bat

--- server/getmoodle.bat
+++ server/getmoodle.bat
@@ -2,7 +2,7 @@
 
 curl --output moodle-latest-310.zip --url https://download.moodle.org/download.php/direct/stable310/moodle-latest-310.zip
 
-tar -xfv moodle-latest-310.zip
+tar -xf moodle-latest-310.zip
 
 :: DEL moodle-latest-310.zip
 
