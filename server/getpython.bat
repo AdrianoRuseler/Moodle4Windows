056319e4879e00a53af931f3cb2d848ba3b452a8
Adriano Ruseler <ruseler@utfpr.edu.br>
2021-04-20 17:23:33 -0300

    Update getpython.bat

--- server/getpython.bat
+++ server/getpython.bat
@@ -1,11 +1,10 @@
 
-
-curl --output python-3.9.1-embed-amd64.zip --url https://www.python.org/ftp/python/3.9.1/python-3.9.1-embed-amd64.zip
+curl --output python-embed-amd64.zip --url https://www.python.org/ftp/python/3.9.4/python-3.9.4-embed-amd64.zip
 
 MKDIR python
 
-tar -xvf python-3.9.1-embed-amd64.zip -C python
+tar -xvf python-embed-amd64.zip -C python
 
-DEL python-3.9.1-embed-amd64.zip
+DEL python-embed-amd64.zip
 
 PAUSE
